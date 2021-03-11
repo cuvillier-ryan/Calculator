2498bf8e9515ace34197693042b8c6b3557843c6
cuvillier.ryan@gmail.com <cuvillier.ryan@gmail.com>
2021-03-10 19:15:12 -0600

    result added

--- calculator.js
+++ calculator.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 
 //We need body-parser to handle post request on index.html
@@ -7,19 +6,27 @@ const bodyParser = require("body-parser");
 const app = express();
 
 //Below is the code that will be used whenever we use bodyParser.
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/", function(req, res){
-  res.send("Thanks for posting that!")
+app.post("/", function(req, res) {
+
+  var num1 = Number(req.body.num1);
+  var num2 = Number(req.body.num2);
+
+  var result = num1 + num2;
+
+  res.send("The result of the calculation is "  + result);
 })
 
 
 
 
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log('App listening on port 3000');
 })
